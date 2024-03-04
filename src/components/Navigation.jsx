4809e9ad49b9cd217c320e842419c423618cfe0e
James Stott <japstott@gmail.com>
2024-03-04 07:03:09 +0000

    add comments to explain aspects of navigation bar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+// This functions as the header component that will be rendered across the entire site.
 export default function Navigation(props) {
   return (
     <header className="d-flex justify-content-center py-3">
       <ul className="nav nav-pills">
         <li className="nav-item">
+          {/* Add active class to navigation element if it is the currently navigated page in react router */}
           <NavLink
             to="/"
             className={({ isActive }) =>
