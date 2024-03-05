b237804f7734074dcb06d9f6fd9a3f756b7c4164
James Stott <japstott@gmail.com>
2024-03-05 09:46:08 +0000

    update gallery project caards to use short description of project

--- src/components/GalleryProjectCard.jsx
+++ src/components/GalleryProjectCard.jsx
@@ -5,12 +5,12 @@ export default function GalleryProjectCard({ project }) {
     <div className="col">
       <div className="card h-100">
         <div
-          className="card-img-top gallery-project-image"
+          className="card-img-top gallery-project-card-image"
           style={{ backgroundImage: `url(${project.img.src})` }}
         ></div>
-        <div className="card-body">
+        <div className="card-body gallery-project-card-body">
           <h5 className="card-title">{project.title}</h5>
-          <p className="card-text">{project.description}</p>
+          <p className="card-text">{project.shortDescription}</p>
         </div>
       </div>
     </div>
