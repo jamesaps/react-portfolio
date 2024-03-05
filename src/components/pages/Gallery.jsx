35d8c4cf33e170f23e5b0f9220580aadda7c8618
James Stott <japstott@gmail.com>
2024-03-05 07:27:04 +0000

    create gallery grid container

--- src/components/pages/Gallery.jsx
+++ src/components/pages/Gallery.jsx
@@ -2,8 +2,15 @@ import React from "react";
 
 export default function Gallery() {
   return (
-    <>
-      <div>Gallery</div>
-    </>
+    <div className="container">
+      <h1 className="display-5 fw-bold text-body-emphasis text-center my-5">
+        Gallery
+      </h1>
+
+      <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+
+      </div>
+    </div>
   );
 }
+
