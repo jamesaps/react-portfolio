6fb15558400408bf13c99857cce8a61d06fa4545
James Stott <japstott@gmail.com>
2024-03-05 09:08:59 +0000

    improve sizing and positioning of gallery project card image

--- src/components/GalleryProjectCard.jsx
+++ src/components/GalleryProjectCard.jsx
@@ -3,12 +3,11 @@ import React from "react";
 export default function GalleryProjectCard({ project }) {
   return (
     <div className="col">
-      <div className="card">
-        <img
-          src={project.img.src}
-          className="card-img-top"
-          alt={project.img.alt}
-        />
+      <div className="card h-100">
+        <div
+          className="card-img-top gallery-project-image"
+          style={{ backgroundImage: `url(${project.img.src})` }}
+        ></div>
         <div className="card-body">
           <h5 className="card-title">{project.title}</h5>
           <p className="card-text">{project.description}</p>
