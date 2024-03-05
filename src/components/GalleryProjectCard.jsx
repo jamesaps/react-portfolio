f6701595cd93a8a0bfa47374f51aaedda86c8800
James Stott <japstott@gmail.com>
2024-03-05 12:11:54 +0000

    add interactive functionality to gallery and haved detailed modal view appear when a project is clicked

--- src/components/GalleryProjectCard.jsx
+++ src/components/GalleryProjectCard.jsx
@@ -1,18 +1,38 @@
 import React from "react";
+import { motion } from "framer-motion";
 
-export default function GalleryProjectCard({ project }) {
+export default function GalleryProjectCard({
+  project,
+  selectedProjectId,
+  setSelectedProjectId,
+}) {
   return (
-    <div className="col">
-      <div className="card h-100">
-        <div
+    <motion.div
+      className="col"
+      layoutId={`${project.id}`}
+      onClick={() =>
+        selectedProjectId === null && setSelectedProjectId(project.id)
+      }
+    >
+      <motion.div className="card h-100 gallery-project-card">
+        <motion.div
           className="card-img-top gallery-project-card-image"
           style={{ backgroundImage: `url(${project.img.src})` }}
-        ></div>
-        <div className="card-body gallery-project-card-body">
-          <h5 className="card-title">{project.title}</h5>
-          <p className="card-text">{project.shortDescription}</p>
-        </div>
-      </div>
-    </div>
+          layoutId={`image-${project.id}`}
+        ></motion.div>
+        <motion.div className="card-body gallery-project-card-body">
+          <motion.h5 className="card-title" layoutId={`title-${project.id}`}>
+            {project.title}
+          </motion.h5>
+          <motion.p
+            className="card-text"
+            layoutId={`description-${project.id}`}
+          >
+            {project.shortDescription}
+          </motion.p>
+          <motion.div layoutId={`close-${project.id}`} />
+        </motion.div>
+      </motion.div>
+    </motion.div>
   );
 }
---
+++ src/components/GalleryProjectModal.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+
+export default function GalleryProjectModal({ project, setSelectedProjectId }) {
+  const handleClose = () => {
+    setSelectedProjectId(null);
+  };
+
+  return (
+    <>
+      <motion.div className="gallery-project-modal-overlay w-100 h-100 position-absolute top-0 start-0 rounded-4">
+        <motion.div
+          layoutId={`${project.id}`}
+          className="card shadow-lg position-absolute w-75 p-3 top-50 start-50 translate-middle rounded-3"
+        >
+          <motion.div
+            className="gallery-project-modal-close position-absolute"
+            layoutId={`close-${project.id}`}
+            onClick={handleClose}
+          >
+            <FontAwesomeIcon icon={faCircleXmark} size="lg" />
+          </motion.div>
+          <motion.h2
+            className="display-4 text-center"
+            layoutId={`title-${project.id}`}
+          >
+            {project.title}
+          </motion.h2>
+          <motion.div className="row d-flex flex-row w-100 h-100">
+            <motion.div
+              className="col-12 col-lg-6 p-3"
+              layoutId={`image-${project.id}`}
+            >
+              <img src={project.img.src} className="w-100" />
+            </motion.div>
+            <motion.div
+              className="col-12 col-lg-6 p-3"
+              layoutId={`description-${project.id}`}
+            >
+              <motion.h3>Description</motion.h3>
+              <motion.div>{project.description}</motion.div>
+            </motion.div>
+          </motion.div>
+        </motion.div>
+      </motion.div>
+    </>
+  );
+}
